a26ca21cf75cd9349ee6559bb2e663e6a393376c
Adam Ryman <adamryman@gmail.com>
2017-02-05 09:07:07 -0800

    Moved other thing to lib, uses lib. CLOSURES

--- fuck.go
+++ fuck.go
@@ -1,49 +1,92 @@
-package fuck
+package main
 
 import (
 	"bufio"
 	"bytes"
+	"fmt"
 	"io"
 	"os"
+	"os/exec"
+	"strconv"
+	"time"
 
-	// DAVE CHENY
+	"github.com/adamryman/kit/file/line"
+	// Dave Cheny
 	"github.com/pkg/errors"
 )
 
-func DeleteLine(path string, line int) error {
-	f, err := os.Open(path)
-	if err != nil {
-		return errors.Wrap(err, "cannot open incorrect file")
-	}
-	scanner := bufio.NewScanner(f)
-	buffer := bytes.NewBuffer(nil)
-	for i := 0; i < line; i++ {
-		scanner.Scan()
-		scanner.Bytes()
-		_, err := buffer.Write(scanner.Bytes())
-		if err != nil {
-			return errors.Wrap(err, "cannot write to buffer")
-		}
-	}
-	// DO IT
-	scanner.Scan()
-	// SEE YA
-	for scanner.Scan() {
-		_, err := buffer.Write(scanner.Bytes())
-		if err != nil {
-			return errors.Wrap(err, "cannot write to buffer")
-		}
-	}
-	err = f.Close()
-	if err != nil {
-		return errors.Wrap(err, "cannot close incorrect file")
-	}
-	// LETS DO IT
-	f, err = os.Create(path)
-	_, err = io.Copy(f, buffer)
-	if err != nil {
-		return errors.Wrap(err, "cannot overwrite file with correct one")
-	}
+const maxCalls = 100
 
+func main() {
+	var calls int
+	go func() {
+		for {
+			if calls > 100 {
+				fmt.Fprintln(os.Stderr, "May never build...")
+				fmt.Fprintln(os.Stderr, `¯\_(ツ)_/¯`)
+				os.Exit(1)
+			}
+			time.Sleep(time.Second)
+		}
+	}()
+
+	var fuck func()
+	fuck = func() {
+		calls = calls + 1
+		cmd := exec.Command("go", os.Args[1:]...)
+
+		buffer := bytes.NewBuffer(nil)
+		writer := io.MultiWriter(buffer, os.Stderr)
+		cmd.Stderr = writer
+		cmd.Stdout = os.Stdout
+		scanner := bufio.NewScanner(buffer)
+		err := cmd.Run()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			defer fuck()
+		}
+
+		linesRemoved := 0
+		for scanner.Scan() {
+			b := scanner.Bytes()
+
+			file, lineN, err := parseFileLine(b)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+			}
+			if lineN < 1 {
+				continue
+			}
+			err = lineDeleteWrapper(file, lineN-linesRemoved)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+			} else {
+				linesRemoved = linesRemoved + 1
+			}
+		}
+		buffer.Reset()
+	}
+	fuck()
+	fmt.Fprintln(os.Stderr, "You are now a go developer")
+}
+
+func parseFileLine(goOut []byte) (string, int, error) {
+	fileLine := bytes.Split(goOut, []byte{':'})
+	if len(fileLine) < 2 {
+		return "", 0, nil
+	}
+	file := string(fileLine[0])
+	lineN, err := strconv.Atoi(string(fileLine[1]))
+	if err != nil {
+		return "", 0, errors.Wrap(err, "cannot convert line number to int")
+	}
+	return file, lineN, nil
+}
+
+func lineDeleteWrapper(path string, lineN int) error {
+	err := line.Delete(path, lineN)
+	if err != nil {
+		return err
+	}
 	return nil
 }
